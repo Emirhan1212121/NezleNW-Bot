b03b54bd1f0734ac37c6a3c9aa1dc1e712539ef1
Emirhan1212121 <45706109+Emirhan1212121@users.noreply.github.com>
2018-12-14 14:14:23 +0300

    Update yardm (3).js

--- komutlar/yardm (3).js
+++ komutlar/yardm (3).js
@@ -1,65 +1,47 @@
 const Discord = require('discord.js');
+const ayarlar = require('../ayarlar.json');
 
-exports.run = (client, message, args) => {
+var prefix = ayarlar.prefix;
 
-  let pages = [
-              '**Bot Hakkında Kısa Bilgi**\n\n\n' + '``-`` nt!kurucu Yazarak Botun Kurucusunu Görebilirsiniz.  \n``-`` Sunucunuzda Resimli Giriş Çıkışın Çalışması İçin ``giriş-çıkış`` Adında Bir Metin Kanalı Olmalıdır.\n``-`` nt!otorol = Otorol ayarlamak için etiketlemeden direk rolün ismini yazın!!! \n``-`` Ban,kick,uyar gibi komutları kullanabilmeniz için `ceza-takip-listesi` adlı bir metin kanalı oluştumanız gerek.\n``-`` Eğer nt!pubg/nt!lol/nt!csgo Komutlarını Kullanmak istiyorsanız ``PUBG Gamer - CS:GO Gamer - LOL Gamer `` adlı roller oluşturmanız lazım.',
-              '**Yenilikler**\n\n\n' + '``-``  nt!canlıdestek = Botun Sahibinden Bot İçin ``Destek Talebinde`` Bulunursun.\n``-`` nt!rolinfo = Kullanım şekli nt!rolinfo ROLÜN İSMİ etiketlemeyin! Rol hakkında bilgi verir. \n``-``  nt!öneri = Sizin önerilerinizi Botun kurucusuna iletir! \n``-`` nt!pubg / nt!csgo / nt!lol =Komutlarını yazdığında kişiye bu rollerdeden ``PUBG Gamer - CS:GO Gamer - LOL Gamer `` verir. \n``-``  nt!destekhattım = Size botun destek hattını gönderir. ',
-              '**Kullanıcı Ve Eğlence**\n\n\n' + '``-``  nt!8ball = Bir soru sorun oda size yanıtını versin! \n``-`` nt!inverst = Avatarınızdaki renkleri ters çevirir.\n``-``nt!kasaaç = CS:GO Kasası açar.\n``-`` nt!afk = Afk odasına yönlendirilirsiniz.\n``-`` nt!sunucubilgi = Sunucu Hakkında Bilgi Verir. \n``-``  nt!emojiyazı = Yazdığınız Metni Emojili Bir Şekilde Yazar.\n``-`` nt!öp = Etiketlediğiniz kişiyi öper :heart: .\n``-`` nt!sigaraiç = Sigara içersiniz. \n``-`` nt!ateşet = Birisine Ateş Edersiniz.\n``-`` nt!google = Googlede İstediğnizi aratır ve sonuçları size verir. \n``-`` nt!hackle = Bot Sizi Hackler.  \n``-``  nt!döviz = Euro Ve Doların ``Alış-Satış`` Fiyatını Gösterir. \n``-``  nt!atatürk <sayı> = Atatürkün Rastgele Bir Resmini Atar. \n``-``  nt!avatar = Kullanıcı Resminizi Atar. \n``-``  nt!aşkölçer = Etiketlediğiniz kişiyle aranızdaki aşk seviyesini ölçer.    \n``-`` nt!çekiliş = rastgele bir çekiliş yapar.  \n``-`` nt!havadurumu = Arattığınız şehhrin havadurumunu gösterir.  \n``-``  nt!mesajküçült = Yazdığınız mesajı küçülterek yazar. \n``-``  nt!nitroefekt = Avatarınızı nitro efekti ile gösterir.  \n``-``  nt!roller = Botun bulunduğu sunucudaki rolleri gösterir. \n``-``  nt!servericon = Sunucunun ikonunu gösterir.  \n``-``  nt!slots = Slot oynatır. \n``-``  nt!şifre = Yazdığınız rakama göre rastgele bir şifre oluşturur.  \n``-``  nt!yardım = Komutları gösterir. \n``-``  nt!yazıtura = Yazı Tura oynatır. \n``-``  nt!anket = Belirlediğiniz konu hakkında anket yapar. \n``-``  nt!yaz = Bota istediğiniz şeyi yazdırır. \n``-`` nt!hastebin = Hastebine kod/cümle yüklersiniz. \n``-`` nt!hesapla = Matematik işlemini çözer. \n``-`` nt!1vs1 <@kullanıcı> <@kullanıcı> = Aralarında savaş yaptırırsınız.  \n``-`` nt!wasted = avatarınıza wasted efekti verir.',
-              '**Bot Bilgi ve Admin Komutları**\n\n\n' + '``-``  nt!davetlinkim = Bot İle İlgili Bağlantıları Görürsünüz.\n``-``nt!antispam = Olası bir spama karşı önlem alır.\n``-`` nt!geçiçisustur = Kişiyi geçici olarak susturur. \n``-`` nt!rolinfo = Etiketlediğiniz rol hakkında sizi bilgilendirir. \n``-``nt!kullanıcıbilgim = Komutu yazan kişinin bilgilerini verir. \n``-``  nt!ping = Botun Pingini Gösterir.\n``-`` nt!bottanıt = Botumuzu Tanıtır. \n``-``nt!geçicisustur = Etiketlediğiniz kişiyi susturur. \n``-``  nt!destekhattım = Botun destek hattını gösterir. \n``-``  nt!internetsitesi = Botun internet sitesini gösterir. \n``-``  nt!başvuru = Ekibe katılmanız için başvurunuzu yapar. \n``-`` nt!ban = kişiyi sunucudan sınırsız olarak yasaklar. \n``-`` nt!unban = Kişinin banını kaldırır. \n``-``  nt!reklamtara = Sunucuda kim oynuyor yerinde reklam yapıyorsa gösterir. \n``-``  nt!kick = etiketlediğiniz kişiyi sunucudan atar. \n``-``  nt!linkengelle = Sunucuya atılan linkleri engeller. \n``-`` nt!temizle = Belirttiğiniz miktarda mesaj siler. \n``-`` nt!ses-kanal-aç = Bot yeni bir ses kanalı açar. \n``-`` nt!yavaşmod [1den 100e kadar] = Açtığınızda 1den 100e kadar süre tutabilirsiniz.Kapatmak içinde nt!yavaşmod 0 yazarak kapatabilirsiniz.\n``-`` nt!yazı-kanal-aç =Verdiğiniz isme göre bir kanal oluşturur.',
-              '**Müzik Komutları**\n\n\n' +  '``-`` \n``-`` !!al = Belirttiğiniz şarkıyı çalar..\n``-`` nt!duraklat = Şarkıyı durdurur..\n``-`` nt!devam = Şarkıyı devam ettirir..\n``-`` nt!geç = Bir sonraki şarkıyı çalar.\n``-``  nt!ses <1-50> = Sesi belirttiğiniz miktara göre ayarlar.\n ``-`` nt!çalan = Çalan şarkıyı gösterir.\n``-``  nt!ayrıl = Bot odadan ayrılır.',
-            ];
-              let page = 1;
+exports.run = (client, message, params) => {
+  const embedyardim = new Discord.RichEmbed()
+  .setThumbnail("https://78.media.tumblr.com/10b366f294d47b40d857d6e47872d0dc/tumblr_ntubqoYYsF1sqwlqgo3_250.gif")
+  .setTitle("<a:poliss:502486674844942336>Komutlar <a:polis:502486675449184266> ")
+  .setDescription('')
+  .setColor(0x00ffff)
+  .addField("<a:wavegif:502481554379898881> **Hoşgeldin Mesajı Aktif Etme** <a:wavegif:502481554379898881>",  ` .kur Komutuyla Gerekli Kanalları Oluşturun.`)
+    .addField(":headphones: **Müzik komutları eklendi** <:asbayraklari:502174044871720961> ", `.çal <müzik>`) 
+  .addField("<a:hyperPoggers:502482793155133440> **Eğlence komutları için ** **[.1]**", `Eğlence Komutlarını Gösterir. (.eğlence) `)
+  .addField("<a:hyperPoggers:502482793155133440> **Eğlence komutları 2 için ** **[.4]**", `Eğlence Komutlarının Devamını Gösterir. (.eğlence2) `) 
+  .addField("<a:frograinbow:505984020790902785> **Yetkili komutları için [.2]**", `Yetkili Komutlarını Gösterir. (.yetkili) `) 
+  .addField("<:tr:502174034675499058> **Ana komutlar için** **[.3]**", `Ana Komutları Gösterir. (.anakomutlar) `)
+  .addField("**Komutlar Hakkında Daha Fazla Bilgi Almak İçin** <a:WeeWoo:502333005449134080>", `http://sakir.sitem.xyz/`)
+  .addField("**Discord Sunucumuz:**<a:blobjoining:502485228317835265>",`https://discord.gg/bamkeg7`)
+  .addField("**Tavsiyelerizi Bildirmek İçin `.tavsiye` Komutunu Kullanınız.** <:tr:502174034675499058>",`https://www.youtube.com/c/karmakarisiktv27`)
+   
+  if (!params[0]) {
+    const commandNames = Array.from(client.commands.keys());
+    const longest = commandNames.reduce((long, str) => Math.max(long, str.length), 0);
+    message.channel.send(embedyardim);
+    message.react('✅')
+  } else {
+    let command = params[0];
+    if (client.commands.has(command)) {
+      command = client.commands.get(command);
+      message.author.send('asciidoc', `= ${command.help.name} = \n${command.help.description}\nDoğru kullanım: ` + prefix + `${command.help.usage}`);
+    }
+  }
+};
 
-              const embed = new Discord.RichEmbed()
-                .setColor('RANDOM')
-                .setThumbnail('https://cdn.discordapom/attachments/487719679868272689/488329963926192158/image0.png')
-                .setFooter(`Sayfa ${page} / ${pages.length}`)
-                .setDescription(pages[page-1])
-              message.channel.send(embed).then(msg => {
-            
-              msg.react('⬅')
-              .then(r => {
-                msg.react('➡')
-            
-                  //Filter
-                  const backwardsFilter = (reaction, user) => reaction.emoji.name === '⬅' && user.id === message.author.id;
-                  const forwardsFilter = (reaction, user) => reaction.emoji.name === '➡' && user.id === message.author.id;
-            
-                  const backwards = msg.createReactionCollector(backwardsFilter, { time: 1000000 });
-                  const forwards = msg.createReactionCollector(forwardsFilter, { time: 1000000 });
-            
-                  forwards.on('collect', r => {
-                    if(page === pages.length) return;
-                    page++;
-                    embed.setDescription(pages[page-1]);
-                    embed.setColor('RANDOM')
-                    embed.setFooter(`Sayfa ${page} / ${pages.length}`)
-                    msg.edit(embed)
-                  })
-                  backwards.on('collect', r => {
-                    if(page === 1) return;
-                    page--;
-                    embed.setColor('RANDOM')
-                    embed.setDescription(pages[page-1]);
-                    embed.setFooter(`Sayfa ${page} / ${pages.length}`)
-                    msg.edit(embed)
-                  })
-            
-                })
-              })
-            };
-            
-            
-            exports.conf = {
-            enabled: true,
-            guildOnly: true,
-            aliases: ["help", "y", "h"],
-            permLevel: 0
-            };
-            
-            exports.help = {
-            name: 'yardım',
-            description: 'Yardım Listesini Gösterir',
-            usage: 'yardım'
-            };
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: ['h', 'halp', 'help', 'y'],
+  permLevel: 0
+};
+
+exports.help = {
+  name: 'yardım',
+  description: 'Tüm komutları gösterir.',
+  usage: 'yardım [komut]'
+};
